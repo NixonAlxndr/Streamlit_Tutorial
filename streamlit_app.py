5982c9f6ef28a4b534e9b2f03c2b0c841fdb1d0e
NxN <94671395+NixonAlxndr@users.noreply.github.com>
2025-03-17 14:16:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import joblib
 def main():
   st.title('Dermatology Machine Learning') 
   st.info('This app is using machine learning')
+  erythema = st.slider('Erythema', min_value = 0, max_value = 3, value = 2)
 
 if __name__ == '__main__':
   main()
